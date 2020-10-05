f5b30b311a977fa9656dac3bcd9f419eb1f6ffe5
yoogur <798254213@qq.com>
2020-10-05 20:25:54 +0800

    更新mysqlclient

--- 工具/init.bat
+++ 工具/init.bat
@@ -1,5 +1,5 @@
 python -m pip install --upgrade pip
 pip install pymysql
-pip install mysqlclient-1.4.6-cp38-cp38-win32.whl
+pip install mysqlclient-1.4.6-cp37-cp37m-win_amd64.whl
 pip install django
 pause
