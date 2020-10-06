575d006173df570e9de7cbd5bc83635e0afcd28f
Madalo66 <807563569@qq.com>
2020-10-06 10:02:43 +0800

    测试登录注册接口

---
+++ app/gameforum/src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>home</p>
+    <router-link to="/login">登录</router-link>
+    <router-link to="/register">注册</router-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
